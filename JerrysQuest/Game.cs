0033f97c294a8b4d599e58b52553422ae00271e3
gjorshoskaivana <63347774+gjorshoskaivana@users.noreply.github.com>
2020-06-17 11:02:29 +0200

    add Game load code

---
+++ JerrysQuest/Game.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JerrysQuest
+{
+    public partial class Game : Form
+    {
+        public Game()
+        {
+            InitializeComponent();
+        }
+
+        private void Game_Load(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+
+            List<Control> pb = new List<Control>();
+            foreach (Control c in this.Controls)
+            {
+                if (c.Name.Contains("pictureBox"))
+                {
+                    c.Tag = "wall";
+                    c.Location = new Point(rand.Next(50, 600), rand.Next(50, 300));
+                    pb.Add(c);
+                }
+            }
+        }
+    }
+}
