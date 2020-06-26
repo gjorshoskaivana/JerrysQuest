5166192241d9c9519813ec04420e445d87673ba7
gjorshoskaivana <63347774+gjorshoskaivana@users.noreply.github.com>
2020-06-26 13:51:47 +0200

    cheese collecting

---
+++ JerrysQuest/Cheese.cs
@@ -0,0 +1,30 @@
+﻿using JerrysQuest.Properties;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JerrysQuest
+{
+    public class Cheese
+    {
+        public Image img { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Cheese(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+            this.img = Resources.cheese;
+        }
+
+        public void drawCheese(Graphics g)
+        {
+            g.DrawImage(img, X * Game.SIDE, Y * Game.SIDE, img.Width, img.Height);
+        }
+
+    }
+}
